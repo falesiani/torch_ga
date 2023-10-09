cabf6f0ff7e67a234c4bebd4c0831c428e3cc1d4
Francesco Alesiani <Francesco.Alesiani@neclab.eu>
2023-10-09 12:07:42 +0200

    added version to init file

--- torch_ga/__init__.py
+++ torch_ga/__init__.py
@@ -1,1 +1,11 @@
+"""
+pyexample.
+
+An example python library.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Francesco Alesiani'
+__credits__ = 'TF-GA (https://doi.org/10.5281/zenodo.3902404)'
+
 from .torch_ga import GeometricAlgebra
